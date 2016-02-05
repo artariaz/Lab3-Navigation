6b080241793374a054b4a43e182e558a2195bab3
artariaz <riazrafat.arta@gmail.com>
2016-02-05 15:39:12 -0500

    added methods for Navigator class

--- src/navigation/Navigator.java
+++ src/navigation/Navigator.java
@@ -1,8 +1,21 @@
 package navigation;
 
-public class Navigator {
+public class Navigator extends Thread {
 
 	public Navigator() {
 
+	}
+
+	void travelTo(double x, double y) {
+
+	}
+
+	void turnTo(double theta) {
+
+	}
+
+	boolean isNavigating() {
+
+		return true;
 	}
 }
