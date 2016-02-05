eb1c9e39a0f143a08c949c40d1224ef376b2809a
artariaz <riazrafat.arta@gmail.com>
2016-02-05 14:13:39 -0500

    Repo added to github

---
+++ src/navigation/Lab3.java
@@ -0,0 +1,21 @@
+package navigation;
+
+/*
+ * Group: 41
+ * Katy Dong      (260610798)
+ * Arta Riazrafat (260636821)
+ */
+
+public class Lab3 {
+
+	// Parameters: adjust these for desired performance
+	
+	
+	// Static Resources:
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
