c8ae0605543246ea13768996c43403d515eb2a3e
artariaz <riazrafat.arta@gmail.com>
2016-02-06 12:15:42 -0500

    changes to Navigator class

--- src/navigation/Navigator.java
+++ src/navigation/Navigator.java
@@ -7,35 +7,43 @@ public class Navigator extends Thread {
 	private static final int ROTATE_SPEED = 150;
 	private EV3LargeRegulatedMotor rightMotor;
 	private EV3LargeRegulatedMotor leftMotor;
+	private boolean state = false;
+	private Odometer odometer;
 
-	public Navigator(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor) {
+	public Navigator(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, Odometer odometer) {
 		this.rightMotor = rightMotor;
 		this.leftMotor = leftMotor;
-		
+		this.odometer = odometer;
+
 	}
 
 	public void run() {
-		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] {
-				leftMotor, rightMotor }) {
+		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] { leftMotor, rightMotor }) {
 			motor.stop();
 			motor.setAcceleration(3000);
 		}
-		travelTo(60,30);
-		travelTo(30,30);
-		travelTo(30,60);
-		travelTo(60,0);
+		travelTo(60, 30);
+		travelTo(30, 30);
+		travelTo(30, 60);
+		travelTo(60, 0);
 	}
 
 	void travelTo(double x, double y) {
-		
+
 	}
 
-	void turnTo(double theta) {
+	void turnTo(double desiredTheta) {
+		double theta = this.odometer.getTheta();
 
+		if (theta > desiredTheta) {
+			// Turn right
+		} else if (theta < desiredTheta) {
+			// Turn left
+		}
 	}
 
 	boolean isNavigating() {
 
-		return true;
+		return this.state;
 	}
 }
