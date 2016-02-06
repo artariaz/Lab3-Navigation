f72050edf8770c9c685904e6f1803c6fa0a93e6e
artariaz <riazrafat.arta@gmail.com>
2016-02-06 15:18:46 -0500

    Added boolean methods and completed turnTo

--- src/navigation/Navigator.java
+++ src/navigation/Navigator.java
@@ -11,32 +11,35 @@ public class Navigator extends Thread {
 	private Odometer odometer;
 	private double rightRadius, leftRadius, width;
 	private double destX, destY, destAngle;
+	private double error = 5;
 
-	enum State {INIT, TURNING, TRAVELLING};
-	
-	public Navigator(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, Odometer odometer, double rightRadius, double leftRadius, double width) {
+	enum State {
+		INIT, TURNING, TRAVELLING
+	};
+
+	public Navigator(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, Odometer odometer,
+			double rightRadius, double leftRadius, double width) {
 		this.rightMotor = rightMotor;
 		this.leftMotor = leftMotor;
 		this.odometer = odometer;
 		this.leftRadius = leftRadius;
 		this.rightRadius = rightRadius;
-		this.width = width; 
+		this.width = width;
 
 	}
-	
+
 	public void run() {
 		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] { leftMotor, rightMotor }) {
 			motor.stop();
 			motor.setAcceleration(3000);
 		}
 
-		
-		travelTo(60,30);
-		travelTo(30,30);
-		travelTo(30,60);
-		travelTo(60,0);
+		travelTo(60, 30);
+		travelTo(30, 30);
+		travelTo(30, 60);
+		travelTo(60, 0);
 	}
-	
+
 	public double getDestAngle() {
 		double currentX = this.odometer.getX();
 		double currentY = this.odometer.getY();
@@ -49,8 +52,6 @@ public class Navigator extends Thread {
 		return destAngle;
 	}
 
-	
-
 	void travelTo(double x, double y) {
 		this.destX = x;
 		this.destY = y;
@@ -58,51 +59,82 @@ public class Navigator extends Thread {
 		this.isNavigating = true;
 
 	}
-	
-	public boolean checkIfDone(double [] position) {
-		return true;
+
+	public boolean checkIfDone(double x, double y) {
+		// x and y are the odometer's readings
+		// Compare with destX and destY with a degree of tolerance
+		// And return true if they are close to the desired values
+		if (destX + error >= x || destX - error <= x || destY + error >= y || destY - error <= y) {
+			return true;
+		} else
+			return false;
 	}
-	
+
 	public boolean facingDest() {
-		return true;
+		double currentAngle = this.odometer.getTheta();
+
+		// If angle is near the desired angle given a tolerance error, return
+		// true
+		if (destAngle + error >= currentAngle || destAngle - error <= currentAngle) {
+			return true;
+		} else
+			return false;
 	}
-	
-	
+
 	void updateTravel() {
 		rightMotor.setSpeed(FORWARD_SPEED);
 		leftMotor.setSpeed(FORWARD_SPEED);
-		
+
 		rightMotor.forward();
 		leftMotor.forward();
 	}
 
-	void turnTo(double desiredTheta) {
-		double theta = this.odometer.getTheta();
-		double rotationTheta;
-		double smallestTheta;
-		if (theta > desiredTheta) {
-			rotationTheta = theta - desiredTheta;
-			if (rotationTheta > 180) {
-				// Turn left by 360 - rotationTheta
-				smallestTheta = 360 - rotationTheta;
-			} else {
-				// Turn right by rotationTheta
-				smallestTheta = rotationTheta;
+	void turnTo(double desiredAngle) {
+		double currentAngle = this.odometer.getTheta();
+		double rotationAngle;
+		double smallestAngle;
+		if (currentAngle > desiredAngle) {
+			rotationAngle = currentAngle - desiredAngle;
+			if (rotationAngle > 180) {
+				// Turn left by 360 - rotationAngle
+				smallestAngle = 360 - rotationAngle;
 				leftMotor.setSpeed(ROTATE_SPEED);
 				rightMotor.setSpeed(ROTATE_SPEED);
-
-		//		leftMotor.rotate(convertAngle(leftRadius, width, 90.0), true);
-		//		rightMotor.rotate(-convertAngle(rightRadius, width, 90.0), false);
+				leftMotor.rotate(-convertAngle(leftRadius, width, smallestAngle),
+				true);
+				rightMotor.rotate(convertAngle(rightRadius, width, smallestAngle),
+				false);
+			} else {
+				// Turn right by rotationAngle
+				smallestAngle = rotationAngle;
+				leftMotor.setSpeed(ROTATE_SPEED);
+				rightMotor.setSpeed(ROTATE_SPEED);
+				leftMotor.rotate(convertAngle(leftRadius, width, smallestAngle),
+				true);
+				rightMotor.rotate(-convertAngle(rightRadius, width, smallestAngle),
+				false);
 			}
 
-		} else if (theta < desiredTheta) {
-			rotationTheta = desiredTheta - theta;
-			if (rotationTheta > 180) {
+		} else if (currentAngle < desiredAngle) {
+			rotationAngle = desiredAngle - currentAngle;
+			if (rotationAngle > 180) {
 				// Turn right by 360 - rotationTheta
-				smallestTheta = 360 - rotationTheta;
+				smallestAngle = 360 - rotationAngle;
+				leftMotor.setSpeed(ROTATE_SPEED);
+				rightMotor.setSpeed(ROTATE_SPEED);
+				leftMotor.rotate(convertAngle(leftRadius, width, smallestAngle),
+				true);
+				rightMotor.rotate(-convertAngle(rightRadius, width, smallestAngle),
+				false);
 			} else {
 				// Turn left by rotationTheta
-				smallestTheta = rotationTheta;
+				smallestAngle = rotationAngle;
+				leftMotor.setSpeed(ROTATE_SPEED);
+				rightMotor.setSpeed(ROTATE_SPEED);
+				leftMotor.rotate(-convertAngle(leftRadius, width, smallestAngle),
+				true);
+				rightMotor.rotate(convertAngle(rightRadius, width, smallestAngle),
+				false);
 			}
 		}
 
@@ -112,7 +144,7 @@ public class Navigator extends Thread {
 
 		return this.isNavigating;
 	}
-	
+
 	private static int convertDistance(double radius, double distance) {
 		return (int) ((180.0 * distance) / (Math.PI * radius));
 	}
